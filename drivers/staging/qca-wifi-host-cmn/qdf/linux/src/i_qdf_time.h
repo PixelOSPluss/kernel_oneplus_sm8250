d55690d656aa34429b147ce2a4ccdc81e8bef9c5
zenzer0s <praveenzalaki200@gmail.com>
2026-08-12 18:34:28 +0000

    qdf: use ktime_to_ns(ktime_get_boottime()) in __qdf_get_bootbased_boottime_ns

--- drivers/staging/qca-wifi-host-cmn/qdf/linux/src/i_qdf_time.h
+++ drivers/staging/qca-wifi-host-cmn/qdf/linux/src/i_qdf_time.h
@@ -309,13 +309,13 @@ static inline uint64_t __qdf_get_log_timestamp(void)
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 3, 0))
 static inline uint64_t __qdf_get_bootbased_boottime_ns(void)
 {
-	return ktime_get_boottime_ns();
+	return ktime_to_ns(ktime_get_boottime());
 }
 
 #elif (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 17, 0))
 static inline uint64_t __qdf_get_bootbased_boottime_ns(void)
 {
-	return ktime_get_boottime_ns();
+	return ktime_to_ns(ktime_get_boottime());
 }
 
 #else
